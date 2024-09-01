33d91ada7efc9339756e5acfdbdf4c45eab6b1cf
Alexander <17806067729@163.com>
2024-09-01 21:08:23 +0800

    2024.9.1_014

--- app.py
+++ app.py
@@ -1,39 +1,33 @@
-import streamlit as st
-import os
-
-
-def pages():
-    pages = {
-        '主页': 'main_page.py',
-        '网页设计': 'Web_Design.md',
-        'Fig_preservation': {
-            '项目信息': os.path.join('Fig_preservation','information.md'),
-            '实验设计': os.path.join('Fig_preservation','experi_design.md')
-        }
-    }
-
-    page_name = st.sidebar.radio('导航', list(pages.keys()))
-    page_file = None
-
-    if page_name == '主页':
-        page_file = pages[page_name]
-    elif page_name == '网页设计':
-        page_file = pages[page_name]
-    elif page_name == 'Fig_preservation':
-        fig_preservation = pages[page_name]
-        page_title = st.sidebar.radio('分类', list(fig_preservation.keys()))
-        page_file = fig_preservation[page_title]
-    
-    if page_file:
-        try:
-            with open(page_file, encoding='utf-8') as file:
-                exec(file.read())
-        except FileNotFoundError:
-            st.write(f'文件 {page_file} 找不到')
-        except Exception as e:
-            st.write(f'执行文件时出错: {e}')
-    else:
-        st.write('所选页面不正确')
-
-
+import streamlit as st  
+import os  
+  
+def pages():  
+    pages = {  
+        '主页': 'main_page.py',  
+        '网页设计': 'Web_Design.md',  
+        'Fig_preservation': {  
+            '项目信息': os.path.join('Fig_preservation', 'information.md'),  
+            '实验设计': os.path.join('Fig_preservation', 'experi_design.md')  
+        }  
+    }  
+  
+    page_name = st.sidebar.radio('导航', list(pages.keys()))  
+    page_file = None  
+  
+    if page_name == '主页':  
+        page_file = pages[page_name]  
+        # 如果主页是 Python 脚本，你可能需要特别处理它  
+        if page_file.endswith('.py'):  
+            with open(page_file, encoding='utf-8') as file:  
+                exec(file.read())  
+    elif page_name == '网页设计' or page_name.startswith('Fig_preservation'):  
+        page_file = pages[page_name] if not isinstance(pages[page_name], dict) else pages[page_name][st.sidebar.radio('分类', list(pages[page_name].keys()))]  
+        if page_file.endswith('.md'):  
+            with open(page_file, encoding='utf-8') as file:  
+                md_content = file.read()  
+                st.markdown(md_content)  
+  
+    if not page_file or (not page_file.endswith('.py') and not page_file.endswith('.md')):  
+        st.write('所选页面不正确或文件类型不支持')  
+  
 pages()
