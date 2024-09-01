b1210722d0b59c648c4bfd2d20a551ab9ecd3fe5
Alexander <17806067729@163.com>
2024-09-01 20:54:51 +0800

    2024.9.1_007

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 import streamlit as st
 import os
-st.title('陈浩实验室')
 
-st.write('__欢迎访问，这里是山东大学威海校区海洋学院陈浩老师实验室。__')
-
-def load_markdown_file(file_name):  
-    with open(file_name, 'r', encoding='utf-8') as file:  
-        return file.read()  
-  
-# 将 MD 文件内容显示在 Streamlit 网页上  
-md_content = load_markdown_file("Web_Design.md")  
-st.markdown(md_content)  
 
 def pages():
     pages = {
+        '主页': 'main_page.py',
+        '网页设计': 'Web_Design.md',
         'Fig_preservation': {
-            'Fig_presrvation': 'information.py'
+            'information': 'information.md',
+            'experi_design': 'experi_design.md'
         }
     }
 
     page_name = st.sidebar.radio('导航', list(pages.keys()))
     page_file = None
 
-    if page_name == 'Fig_preservation':
+    if page_name == '主页':
+        page_file = pages[page_name]
+    elif page_name == '网页设计':
+        page_file = pages[page_name]
+    elif page_name == 'Fig_preservation':
         fig_preservation = pages[page_name]
         page_title = st.sidebar.radio('分类', list(fig_preservation.keys()))
         page_file = fig_preservation[page_title]
