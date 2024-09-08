d23bb4f89744299fb927b1e60148deeb7eb78e59
Alexander <17806067729@163.com>
2024-09-08 19:13:53 +0800

    2024.9.8_029

--- Cloud_storage.py
+++ Cloud_storage.py
@@ -54,8 +54,42 @@ def download_file():
     else:
         st.write('没有文件可供下载')
 
+def update_file():
+    """处理文件更新"""
+    st.subheader('更新文件')
+    files = list_files()
+    file_name = st.selectbox('选择要更新的文件', files)
+    
+    if file_name:
+        uploaded_file = st.file_uploader("选择新的文件来替换", type=['zip', 'csv', 'txt', 'pdf', 'png', 'jpg', 'jpeg'])
+        if uploaded_file:
+            if uploaded_file.type == 'application/zip':
+                # 如果上传的是ZIP文件
+                with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
+                    for file_info in zip_ref.infolist():
+                        file_data = zip_ref.read(file_info.filename)
+                        bucket.put_object(file_info.filename, file_data)
+                        st.write(f'文件 {file_info.filename} 已被更新')
+                st.success('ZIP文件中的所有文件已成功更新到 OSS')
+            else:
+                # 更新单个文件
+                bucket.put_object(uploaded_file.name, uploaded_file)
+                st.success(f'文件 {uploaded_file.name} 已被更新')
+
+def delete_file():
+    """处理文件删除"""
+    st.subheader('删除文件')
+    files = list_files()
+    file_name = st.selectbox('选择要删除的文件', files)
+    
+    if file_name and st.button('删除'):
+        bucket.delete_object(file_name)
+        st.success(f'文件 {file_name} 已成功删除')
+
 def cloud_storage_page():
     """显示云存储页面"""
     st.title("云存储")
     upload_files()
     download_file()
+    update_file()
+    delete_file()
