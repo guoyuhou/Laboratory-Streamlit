2d7979fef35b4d016be0fda0ad70b1abed3da60a
Alexander <17806067729@163.com>
2024-09-08 19:08:25 +0800

    2024.9.8_028

--- Cloud_storage.py
+++ Cloud_storage.py
@@ -17,37 +17,45 @@ def list_files():
     """列出OSS中的所有文件"""
     return [obj.key for obj in oss2.ObjectIterator(bucket)]
 
-def upload_zip_file():
-    """处理ZIP文件上传和解压"""
-    st.subheader('上传文件夹（压缩为ZIP）')
-    uploaded_file = st.file_uploader("选择要上传的ZIP文件", type='zip')
+def upload_files():
+    """处理文件上传"""
+    st.subheader('上传文件')
+    
+    uploaded_file = st.file_uploader("选择要上传的文件（支持ZIP和其他类型）", type=['zip', 'csv', 'txt', 'pdf', 'png', 'jpg', 'jpeg'])
+    
     if uploaded_file:
-        # 解压ZIP文件并上传到OSS
-        with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
-            for file_info in zip_ref.infolist():
-                # 读取文件数据
-                file_data = zip_ref.read(file_info.filename)
-                # 上传到OSS
-                bucket.put_object(file_info.filename, file_data)
-                st.write(f'文件 {file_info.filename} 上传成功')
-        st.success('所有文件已成功上传到 OSS')
+        if uploaded_file.type == 'application/zip':
+            # 如果上传的是ZIP文件
+            with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
+                for file_info in zip_ref.infolist():
+                    file_data = zip_ref.read(file_info.filename)
+                    bucket.put_object(file_info.filename, file_data)
+                    st.write(f'文件 {file_info.filename} 上传成功')
+            st.success('ZIP文件中的所有文件已成功上传到 OSS')
+        else:
+            # 处理其他文件类型
+            bucket.put_object(uploaded_file.name, uploaded_file)
+            st.success(f'文件 {uploaded_file.name} 上传成功')
 
 def download_file():
     """处理文件下载"""
     st.subheader('下载文件')
     files = list_files()
-    file_name = st.selectbox('选择要下载的文件', files)
-    if file_name and st.button('下载'):
-        obj = bucket.get_object(file_name)
-        st.download_button(
-            label='下载文件',
-            data=obj.read(),
-            file_name=file_name,
-            mime='application/octet-stream'
-        )
+    if files:
+        file_name = st.selectbox('选择要下载的文件', files)
+        if file_name and st.button('下载'):
+            obj = bucket.get_object(file_name)
+            st.download_button(
+                label='下载文件',
+                data=obj.read(),
+                file_name=file_name,
+                mime='application/octet-stream'
+            )
+    else:
+        st.write('没有文件可供下载')
 
 def cloud_storage_page():
     """显示云存储页面"""
     st.title("云存储")
-    upload_zip_file()
+    upload_files()
     download_file()
