1538b6cead39afd19716f793edf348d27870090b
Alexander <17806067729@163.com>
2024-09-19 21:12:13 +0800

    2024.9.19_020

--- app.py
+++ app.py
@@ -114,27 +114,16 @@ class PageManager:
             st.markdown("### 可访问项目")
             project_options = []
 
-            if user['role'] == '导师':
-                project_options = [
-                    (u, data.get('projects', [])) 
-                    for u, data in self.users.items() if data['role'] in ['研究生', '本科生']
-                ]
-            elif user['role'] == '研究生':
-                project_options = [
-                    (u, data.get('projects', [])) 
-                    for u, data in self.users.items() if data['role'] == '本科生'
-                ]
-            elif user['role'] == '本科生':
-                project_options = [(username, user.get('projects', []))]
+            # 收集所有用户及其项目
+            for u, data in self.users.items():
+                for project in data.get('projects', []):
+                    project_options.append(f"{u}: {project}")
 
-            # 使用下拉框选择用户
-            selected_user = st.selectbox("选择用户查看项目", [u[0] for u in project_options])
+            # 使用下拉框选择项目
+            selected_project = st.selectbox("选择项目查看", project_options)
 
-            # 展示所选用户的项目
-            for user_tuple in project_options:
-                if user_tuple[0] == selected_user:
-                    for project in user_tuple[1]:
-                        st.write(f"- 用户: {user_tuple[0]}, 项目: {project}")
+            # 展示所选项目
+            st.write(f"您选择的项目: {selected_project}")
 
 # Main Application
 def main():
