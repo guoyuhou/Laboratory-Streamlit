ed3b7d9a4894eb53523a9eab4394e5f930a13104
Alexander <17806067729@163.com>
2024-09-22 15:10:02 +0800

    2024.9.22_053

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def handle_login(auth_manager):
         if username and password:
             user = auth_manager.authenticate_user(username, password)
             if user:
-                st.
+                st.balloons()
                 st.session_state.update({'username': username, 'role': user['role'], 'login_page': False})
             else:
                 st.error("用户名或密码无效")
