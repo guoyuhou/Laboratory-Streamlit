6a27e90b97a6567d95f5deec869716388c017a5d
Alexander <17806067729@163.com>
2024-09-22 18:13:52 +0800

    2024.9.22_094

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def main():
     else:
         page_manager = PageManager(st.session_state['role'], users, auth_manager)
         page_manager.display_pages()
-    st.video('videos/elon_mask2.mp4')
+    st.video('videos/elon_mask.mp4')
 
     # 侧边栏内容
     with st.sidebar:
